b1055187dd466cd6cc546e3be279b8bdafbc440e
RuhanaAzam <azam.ruhana@gmail.com>
2024-07-18 03:24:02 -0700

    Added validaor_agent files to this commit

---
+++ test/validation_agent/prompts.py
@@ -0,0 +1,130 @@
+prompt__validate_action: str = lambda task_action: f"""# Task
+You are an RPA bot that navigates digital UIs like a human. Your job is to validate that a certain action was successfully taken.
+
+# Action
+The action that was supposed to be taken was: "{task_action}"
+
+# Question
+
+The first screenshot shows the digital UI BEFORE the action was supposedly taken.
+The second screenshot shows the digital UI AFTER the action was supposedly taken.
+
+Given the change between the screenshots, was the action successfully taken? Be lenient and assume that the action was taken if the UI is "close enough" to the expected UI.
+
+Answer in the JSON format:
+{{
+    "rationale": <rationale>,
+    "was_taken": <true/false>
+}}
+
+Answer:"""
+
+prompt__validate_with_vision__intro: str = lambda task_descrip: f"""# Task
+Your job is to decide whether the workflow was successfully completed, as depicted by the following sequence of screenshots.
+
+# Workflow
+
+The workflow is: "{task_descrip}"
+
+# User Interface
+
+The workflow was executed within the web application shown in the screenshots.
+
+# Workflow Demonstration
+
+You are given the following sequence of screenshots which were sourced from a demonstration of the workflow. 
+The screenshots are presented in chronological order.
+
+Here are the screenshots of the workflow:"""
+
+prompt__validate_with_vision__close: str = lambda : f"""
+# Instructions
+
+Given what you observe in the previous sequence of screenshots, was the workflow successfully completed? 
+To determine this, derive few visual questions from the task description that upon answering will help decide if the workflow was successfully completed.
+If the workflow is asking a question, consider it completed successfully if you could deduce the answer to the question by viewing the screenshots. 
+If the workflow was completed successfully, then set `was_completed` to `true`.
+Also, provide the visual questions and their answers as part of the response.
+
+Provide your answer as a JSON dictionary with the following format:
+{{
+    "visual_questions": <list of visual questions and their answers>,
+    "rationale": <rationale>,
+    "was_completed": <true/false>
+}}
+
+Please write your JSON below:
+"""
+
+prompt__validate_with_text__intro: str = lambda task_descrip: f"""# Task
+Your job is to decide whether the workflow was successfully completed, as depicted by the following sequence of actions.
+
+# Workflow
+
+The workflow is: "{task_descrip}"
+
+# User Interface
+
+The workflow was executed within the web application. After each step, a description of the action is given.
+
+# Workflow Demonstration
+
+You are given the following sequence of actions which summarizes each part of the demonstration of the workflow. 
+The actions are presented in chronological order.
+
+Here are the actions from the workflow:"""
+
+prompt__validate_with_text__close: str = lambda : f"""
+# Instructions
+
+Given what you observe in the previous sequence of action descriptions, was the workflow successfully completed? 
+To determine this, derive few questions from the task description that upon answering will help decide if the workflow was successfully completed.
+If the workflow is asking a question, consider it completed successfully if you could deduce the answer to the question by viewing the sequence of actions. 
+If the workflow was completed successfully, then set `was_completed` to `true`. Please only set `was_completed` to false if you are absolutely certain the solution provided for the task is wrong. Some workflows will only contain the answer in a small portion of the workflow or in the final answer only. Keep this in mind.
+Also, provide the questions and their answers as part of the response.
+
+Provide your answer as a JSON dictionary with the following format:
+{{
+    "reasoning_questions": <list of questions and their answers>,
+    "rationale": <rationale>,
+    "was_completed": <true/false>
+}}
+
+Please write your JSON below:
+"""
+
+prompt__validate_with_text_vision__intro: str = lambda task_descrip: f"""# Task
+Your job is to decide whether the workflow was successfully completed, based off the evaluations of a text evaluator and vision evaluator. The text evaluator is exclusively given text information about the workflow. The vision evaluator is exclusively given screenshots throughout the workflow. You are given the outcome based of each of these evaluator. Note there are certain situations where one evaluators works better than the other:
+
+Text Evaluator Advantages: The text evaluator includes a summary of every action of the workflow with no missing parts. This means every step of the way is captured with no gaps.
+Text Evaluator  Disadvantages: The text evaluator only summarizes each step based off the site DOM. These summaries are sometimes inaccurate as some actions cannot be verified by the DOM. Thus, occasionally the text evaluator assumes a task was done correctly when in fact it was not. 
+Vision Evaluator Advantages: The vision evaluator has an accurate depiction of the website with a single screenshot. Unlike the text evaluator, it is always an accurate depiction of the current state of the website.
+Vision Evaluator Disadvantages: The vision evaluator frequently is provided with little to no screenshots, thus in many cases it does not have enought information to evaluate the workflow.
+
+
+# Workflow
+
+The workflow is: "{task_descrip}"
+
+# User Interface
+
+The workflow was executed within the web application. The text evaluator and vision evaluator give their assesment of wheather the task was successfully completed as well as their reasoning.
+"""
+
+prompt__validate_with_text_vision__close: str = lambda : f"""
+# Instructions
+
+Given what you the assesements given by the text and vision evaluator, was the workflow successfully completed? 
+To determine this, derive few questions from the task description that upon answering will help decide if the workflow was successfully completed.
+If the workflow was completed successfully, then set `was_completed` to `true` otherwise set to `false`. In cases where many screenshots are missing and it cannot be confirmed if a task was done successfully, rely on the text evaluator. Rely on the visual evaluator when it can absolutely confirm the text evaluator is wrong. With the information given, please determine if the the intial task was completed by the workflow.
+
+
+Provide your answer as a JSON dictionary with the following format:
+{{
+    "reasoning_questions": <list of questions and their answers>,
+    "rationale": <rationale>,
+    "was_completed": <true/false>
+}}
+
+Please write your JSON below:
+"""
---
+++ test/validation_agent/validator.py
@@ -0,0 +1,248 @@
+from .prompts import (
+    prompt__validate_action, 
+    prompt__validate_with_vision__intro, prompt__validate_with_vision__close, 
+    prompt__validate_with_text__intro, prompt__validate_with_text__close, 
+    prompt__validate_with_text_vision__intro, prompt__validate_with_text_vision__close
+)
+from .utils import (
+    _fetch_openai_completion,
+    fetch_openai_vision_completion,
+    load_screenshot_for_state,
+    build_screenshot_prompt_sequence,
+    build_text_prompt_sequence
+)
+from typing import Dict, Any, List
+import json
+import argparse
+import os
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path_to_images", type=str, help="Path to task demo folder")
+    parser.add_argument("--is_action", action="store_true", help="If TRUE, then eval on action")
+    parser.add_argument("--is_task_completion", action="store_true", help="If TRUE, then eval on task completion success")
+    parser.add_argument("--use_vqa", action="store_true", help="If TRUE, then use VQA on task completion success")
+    parser.add_argument("--requested_action", type=str, help="Action requested for action validation")
+    parser.add_argument("--task", type=str, help="Description of the task for task completion validation")
+    return parser.parse_known_args()
+
+def validate_action(init_state: Dict[str, Any], requested_action: Dict[str, Any], resultant_state: Dict[str, Any]) -> Dict[str, str]:
+    ## Simple validator function of an action that takes as input the initial state, the requested action, and the resultant state, and determines if it succeeded.
+    path_to_screenshot_before, encoded_image_before = load_screenshot_for_state(init_state)
+    path_to_screenshot_after, encoded_image_after = load_screenshot_for_state(resultant_state)
+    prompt: str = prompt__validate_action(requested_action["action"])
+    pred_raw_response: str = fetch_openai_vision_completion(prompt, [encoded_image_before, encoded_image_after])
+
+    # Evaluate
+    try:
+        pred_json = json.loads(pred_raw_response.replace("```json", "").replace("```", "").strip())
+        pred_rationale: Dict[str, str] = pred_json['rationale']
+        pred_is_met: bool = pred_json['was_taken']
+    except:
+        pred_rationale = None
+        pred_is_met = None
+
+    return {
+        # metadata
+        "init_state_id" : init_state["id"],
+        "action_id" : requested_action["id"],
+        "path_to_screenshot_before": path_to_screenshot_before,
+        "path_to_screenshot_after": path_to_screenshot_after,
+        # gt
+        "requested_action": requested_action["action"],
+        # preds
+        "pred_rationale": pred_rationale,
+        "pred_action_taken" : pred_is_met,
+        "pred_raw_response": pred_raw_response,
+    }
+
+def validate_task_vision(state_seq: List[Any], task: str) -> Dict[str, str]:
+    ## Simple validator function that takes as input the sequence of states and the task, and determines if it succeeded.
+    prompt_sequence = build_screenshot_prompt_sequence(state_seq)
+    intro_prompt: Dict[str, str] = {
+        "role" : "user",
+        "content" : [{
+            "type" : "text",
+            "text" : prompt__validate_with_vision__intro(task)
+        }]
+    }
+    close_prompt: Dict[str, str] = {
+        "role" : "user",
+        "content" : [{
+            "type" : "text",
+            "text" : prompt__validate_with_vision__close()
+        }]
+    }
+    # Feed (S, S', S'', ...) -- i.e. all screenshots at once
+    messages: List[str] = [intro_prompt] + prompt_sequence + [close_prompt]
+    pred_raw_response: str = _fetch_openai_completion(messages, model='gpt-4-vision-preview', temperature=0.0)
+
+    # Evaluate
+    try:
+        pred_json = json.loads(pred_raw_response.replace("```json", "").replace("```", "").strip())
+        pred_rationale: Dict[str, str] = pred_json['rationale']
+        pred_is_met: bool = pred_json['was_completed']
+        pred_questions: List[Any] = pred_json["visual_questions"]
+    except:
+        pred_rationale = None
+        pred_is_met = None
+        pred_questions = None
+
+    return {
+        # metadata
+        "task_description": task,
+        # preds
+        "pred_visual_questions": pred_questions,
+        "pred_rationale": pred_rationale,
+        "pred_task_completed" : pred_is_met,
+        "pred_raw_response": pred_raw_response
+    }
+    
+def validate_task_text_vision(text_result: Dict[str,str], vision_result: Dict[str,str], task: str) -> Dict[str, str]:
+    try:
+        # you need to put the prompt together ruhana
+        intro_prompt: Dict[str, str] = {
+            "role" : "user",
+            "content" : [{
+                "type" : "text",
+                "text" : prompt__validate_with_text_vision__intro(task)
+            }] # type: ignore
+        }
+        
+        close_prompt: Dict[str, str] = {
+            "role" : "user",
+            "content" : [{
+                "type" : "text",
+                "text" : prompt__validate_with_text_vision__close()
+            }] # type: ignore
+        }
+        
+        def was_complete_helper(was_complete: bool)-> str:
+            if not was_complete:
+                return "not"
+            return ""
+        
+        text_prompt = f"The text evaluator believes the task was {was_complete_helper(text_result['pred_task_completed'])} completed. This is because {text_result['pred_rationale'].lower()}. To better understand the text validators', here is a dictionary of question and answers which the text validator has answered: {text_result['pred_reasoning_questions']}."
+        vision_prompt = f"The vision evaluator believes the task was {was_complete_helper(vision_result['pred_task_completed'])} completed. This is because {vision_result['pred_rationale'].lower()}. To better understand the text validators', here is a dictionary of question and answers which the text validator has answered: {vision_result['pred_visual_questions']}."
+        prompt_sequence = build_text_prompt_sequence([text_prompt, vision_prompt])
+        
+        messages: List[str] = [intro_prompt] + prompt_sequence + [close_prompt]
+        pred_raw_response: str = _fetch_openai_completion(messages, model='gpt-4-turbo-preview', temperature=0.0)
+        
+        pred_json = json.loads(pred_raw_response.replace("```json", "").replace("```", "").strip())
+        pred_rationale: Dict[str, str] = pred_json['rationale']
+        pred_is_met: bool = pred_json['was_completed']
+        pred_questions: List[Any] = pred_json["reasoning_questions"]
+    except:
+        pred_rationale = None
+        pred_is_met = None
+        pred_raw_response = None
+        pred_questions = None
+
+    return {
+        # metadata
+        "task_description": task,
+        "pred_reasoning_questions": pred_questions,
+        "pred_rationale": pred_rationale,
+        "pred_task_completed" : pred_is_met,
+        "pred_raw_response": pred_raw_response
+    } # type: ignore    
+    
+def validate_task_raw_vision_text(text_seq: List[Any], vision_seq: List[Any], task: str) -> Dict[str, str]:
+    try:
+        vision_result: Dict[str, str] = validate_task_vqa(vision_seq, task=task)
+        text_result: Dict[str, str] = validate_task_text(text_seq, task=task)
+        result: Dict[str, str] = validate_task_vqa_text(text_result=str(text_result), vision_result=str(vision_result), task=task)
+        return result
+        
+    except:
+        return {
+        # metadata
+        "task_description": task,
+        "pred_rationale": None,
+        "pred_task_completed" : None,
+        "pred_raw_response": None
+    } # type: ignore    
+
+def validate_task_text(state_seq: List[Any], task: str) -> Dict[str, str]:
+    ## Simple validator function that takes as input the sequence of states and the task, and determines if it succeeded.
+    prompt_sequence = build_text_prompt_sequence(state_seq)
+    intro_prompt: Dict[str, str] = {
+        "role" : "user",
+        "content" : [{
+            "type" : "text",
+            "text" : prompt__validate_with_text__intro(task)
+        }] # type: ignore
+    }
+    close_prompt: Dict[str, str] = {
+        "role" : "user",
+        "content" : [{
+            "type" : "text",
+            "text" : prompt__validate_with_text__close()
+        }] # type: ignore
+    }  
+    
+    # Feed (S, S', S'', ...) -- i.e. all screenshots at once
+    messages: List[str] = [intro_prompt] + prompt_sequence + [close_prompt]
+    pred_raw_response: str = _fetch_openai_completion(messages, model='gpt-4-turbo-preview', temperature=0.0)
+
+    # Evaluate
+    try:
+        pred_json = json.loads(pred_raw_response.replace("```json", "").replace("```", "").strip())
+        pred_rationale: Dict[str, str] = pred_json['rationale']
+        pred_is_met: bool = pred_json['was_completed']
+        pred_questions: List[Any] = pred_json["reasoning_questions"]
+    except:
+        pred_rationale = None
+        pred_is_met = None
+        pred_questions = None
+
+    return {
+        # metadata
+        "task_description": task,
+        "pred_reasoning_questions": pred_questions,
+        "pred_rationale": pred_rationale,
+        "pred_task_completed" : pred_is_met,
+        "pred_raw_response": pred_raw_response
+    } # type: ignore
+    
+def main(args): # type: ignore
+    is_action: bool = args.is_action
+    is_task_completion: bool = args.is_task_completion
+    use_vqa: bool = args.use_vqa
+    path_to_images: str = args.path_to_images
+    task: str = args.task
+    requested_action: str = args.requested_action
+    
+    assert sum([is_action, is_task_completion]) == 1, "Must specify EXACTLY ONE of --is_action or --is_task_completion"
+    
+    # Execute eval
+    output_file_name: str = ""
+    if is_action:
+        init_state = {"id":0,"path_to_screenshot":f"{path_to_images}/0.png"}
+        resultant_state = {"id": 2, "path_to_screenshot":f"{path_to_images}/1.png"}
+        requested_action = {"id": 1, "action": requested_action}
+
+        out = validate_action(init_state, requested_action, resultant_state)
+    elif is_task_completion:
+        state_seq = []
+        file_num = 0
+        filelist = [filename for filename in os.listdir(path_to_images) if filename.endswith(".png")]
+        filelist.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+        for file in filelist:
+            if file.endswith(".png"):
+                state_seq.append({"id":file_num, "path_to_screenshot": os.path.join(path_to_images,file)})
+                file_num+=1
+        if use_vqa:
+            print("Using VQA")
+            out = validate_task_vqa(state_seq, task)
+        else:
+            print("Without VQA")
+            out = validate_task(state_seq, task)
+    else:
+        raise ValueError("Must specify either --is_action or --is_task_completion")
+    return out
+
+if __name__ == "__main__":
+    args, __ = parse_args()
+    print(main(args))
