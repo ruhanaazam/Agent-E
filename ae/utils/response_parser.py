49ea5d00d519145a244edd5f30390e4596a02a81
RuhanaAzam <azam.ruhana@gmail.com>
2024-08-04 22:49:39 -0700

    Removed planner specific parsing from parse_response(). This is now handled in the planner agent instead.

--- ae/utils/response_parser.py
+++ ae/utils/response_parser.py
@@ -26,39 +26,7 @@ def parse_response(message: str) -> Dict[str, Any]:
     try:
         json_response = json.loads(message)
     except:
-        # If the response is not a valid JSON, try pass it using string matching. 
-        #This should seldom be triggered
-        if ("plan" in message and "next_step" in message):
-            start = message.index("plan") + len("plan")
-            end = message.index("next_step")
-            json_response["plan"] = message[start:end].replace('"', '').strip()
-        if ("next_step" in message and "terminate" in message):
-            start = message.index("next_step") + len("next_step")
-            end = message.index("terminate")
-            json_response["next_step"] = message[start:end].replace('"', '').strip()
-        if ("terminate" in message and "final_response" in message):
-            start = message.index("terminate") + len("terminate")
-            end = message.index("final_response")
-            matched_string=message[start:end].replace('"', '').strip()
-            if ("yes" in matched_string):
-                json_response["terminate"] = "yes"
-            else:
-                json_response["terminate"] = "no"
-            
-            start=message.index("final_response") + len("final_response")
-            end=len(message)-1
-            json_response["final_response"] = message[start:end].replace('"', '').strip()
-
-        elif ("terminate" in message):
-            start = message.index("terminate") + len("terminate")
-            end = len(message)-1
-            matched_string=message[start:end].replace('"', '').strip()
-            if ("yes" in matched_string):
-                json_response["terminate"] = "yes"
-            else:
-                json_response["terminate"] = "no"
         logging.error(f"Error parsing JSON response {raw_messgae}")
-        # TODO: add robust parser here
     return json_response
 
 def getLastValidationMessage(messages:List[Dict[Any, Any]]):
