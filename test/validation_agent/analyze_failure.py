32b2d40de2a2cceb9c9e2a110017443fd99cd3f1
RuhanaAzam <azam.ruhana@gmail.com>
2024-12-01 13:35:38 -0800

    Updated analyze_failure.py with new prompting

--- test/validation_agent/analyze_failure.py
+++ test/validation_agent/analyze_failure.py
@@ -1,11 +1,4 @@
 from random import seed
-from .prompts import (
-    prompt_validate_with_vision_intro, prompt_validate_with_vision_close, 
-    prompt_validate_with_text_intro, prompt_validate_with_text_close, 
-    prompt_validate_with_text_vision_intro, prompt_validate_with_text_vision_close,
-    prompt_validate_with_vision_final_response_intro, prompt_validate_with_vision_final_response_close,
-    prompt_classifier_intro, prompt_classifier_close
-)
 from .utils import (
     _fetch_openai_completion,
     fetch_openai_vision_completion,
@@ -19,6 +12,46 @@ from datetime import datetime
 from test_utils import robust_json_loader
 
 # TODO: modify the prompt(s)
+
+def prompt_analyze_failure_text_intro(task: str):
+    prompt = """
+    Your job is to analyze why workflow was not completed sucessfully, as depicted by a sequence of actions. The workflow which is provided failed to accomplish it's given task. You are an agent which is helping analyze the reason for the workflow failure.
+
+    # Workflow
+
+    The intented workflow was: "{task_descrip}"
+
+    # User Interface
+
+    The workflow was executed within the web application. You are provided a text chat between a planner agent and the user agent which summarizes the browser output.
+
+    # Workflow Demonstration
+
+    You are given the following sequence of actions performed on a browser, which summarizes each part of the demonstration of the workflow. 
+    The actions are presented in chronological order.
+
+    Here are the chat messages from the workflow:
+    """
+    return prompt
+
+def prompt_analyze_failure_text_close():
+    prompt = """# Instructions
+    Given what you observe in the previous sequence of messages, why was the workflow not completed successfully? Remember that every task is possible to accomplish (for example, if your the task was to "Find a lasagna recipe" and the task failed, it is not because lasagna recipes don't exist but because the agent couldn't navigate the website correctly).
+    
+    Ideally, what type of general capabilities should this web-navigation agent need to improve on to be able to acommplish this task better (e.g. being able to use widgets, use better search keywords)
+    
+    Some key information to help:
+    1. allrecipes.com does not have sorting or filtering features
+
+    Provide your answer as a JSON dictionary with the following format:
+    {{
+        "rationale": <reason task failed>,
+        "improvement": <general system improvement suggestion>,
+    }}
+
+    Please write your JSON below:
+    """
+    return prompt
 
 
 def analyze_failure_vision(state_seq: List[Any], task: str, model: str, final_response: str| None = None) -> Dict[str, str]:
@@ -94,20 +127,20 @@ def analyze_failure_text(state_seq: List[Any], task: str, model: str) -> Dict[st
         "role" : "user",
         "content" : [{
             "type" : "text",
-            "text" : prompt_validate_with_text_intro(task, date_message)
+            "text" : prompt_analyze_failure_text_intro(task)
         }] # type: ignore
     }
     close_prompt: Dict[str, str] = {
         "role" : "user",
         "content" : [{
             "type" : "text",
-            "text" : prompt_validate_with_text_close()
+            "text" : prompt_analyze_failure_text_close()
         }] # type: ignore
     }  
     
     # Feed (S, S', S'', ...) -- i.e. all screenshots at once
     messages: List[str] = [intro_prompt] + prompt_sequence + [close_prompt]
-    pred_raw_response: str = _fetch_openai_completion(messages, model=model, temperature=0.0, seed=1234) # model='gpt-4-turbo-preview'
+    pred_raw_response: str = _fetch_openai_completion(messages, model=model, temperature=0.0, seed=1234)
 
     # Evaluate
     try:
